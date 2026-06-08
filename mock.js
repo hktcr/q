68a19dd1b2fcf379bc159bceed3eb33fc5456a86
Håkan Karlsson <hlg.karlsson@gmail.com>
2026-06-08 13:29:08 +0200

    feat: upgrade to meritvärde calculations and classDiscussionComfort slider

--- mock.js
+++
@@ -1,4 +0,0 @@
-const document = { getElementById: () => ({ style: {}, classList: { add: () => {}, remove: () => {} } }), querySelectorAll: () => [] };
-const window = { location: { search: '' } };
-const URLSearchParams = class { constructor() {} get() { return null; } };
-const fetch = async () => {};
--- test_q.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('klasspuls_glosor.html', 'utf8');
-let match = html.match(/const questions = (\[[\s\S]*?\]);/);
-let questions = eval(match[1]);
-questions.forEach((q, i) => console.log(`Index ${i}: Fråga ${q.num} - ${q.type}`));
